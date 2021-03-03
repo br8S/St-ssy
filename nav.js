6aee925a6325ece6ac9f20f9f80f3f19ab4385b0
br8S <56180503+br8S@users.noreply.github.com>
2021-03-03 12:29:47 -0800

    desktop navbar complete

--- nav.js
+++ nav.js
@@ -3,6 +3,8 @@ var drop_children = document.querySelectorAll('.dropdown');
 
 for (var i = 0; i < nav_children.length; i++) {
     (function(i){
+
+        //TAKES CARE OF MAKING DROP NAV VISIBILE ON HOVER IF ONE IS ON ALL OTHERS OFF
         nav_children[i].addEventListener('mouseover', () => {
             drop_children[i].style.visibility = "visible";
             for(var x = 0; x < nav_children.length; x++){
@@ -10,78 +12,13 @@ for (var i = 0; i < nav_children.length; i++) {
                     drop_children[x].style.visibility = "hidden";
                 }
             }
-
-            drop_children[i].addEventListener('mouseover', () => {
-                drop_children[i].style.visibility = "visible";
-            })
         })
 
-        nav_children[i].addEventListener('mouseenter', () => {
-            drop_children[i].addEventListener('mouseout', () => {
-                drop_children[i].style.visibility = "hidden";
-            })
+        //TAKES CARE OF MAKING HIDDEN
+        
+        drop_children[i].addEventListener('mouseleave', () => {
+            drop_children[i].style.visibility = "hidden";
         })
 
     }(i));
 }
-
-
-/* 
-for (var i = 0; i < nav_children.length; i++) {
-    if(i === 0){
-        nav_children[i].addEventListener('mouseover', () => {
-            document.querySelector('.mens-dropdown').style.visibility = "visible";
-            document.querySelector('.womens-dropdown').style.visibility = "hidden";
-            document.querySelector('.accessories-dropdown').style.visibility = "hidden";
-        })
-
-        document.querySelector('.mens-dropdown').addEventListener('mouseover', () => {
-            document.querySelector('.mens-dropdown').style.visibility = "visible";
-            document.querySelector('.womens-dropdown').style.visibility = "hidden";
-            document.querySelector('.accessories-dropdown').style.visibility = "hidden";
-        })  
-
-        document.querySelector('.mens-dropdown').addEventListener('mouseleave', () => {
-            document.querySelector('.mens-dropdown').style.visibility = "hidden";
-        }) 
-    }
-
-    if(i === 1){
-        nav_children[i].addEventListener('mouseover', () => {
-            document.querySelector('.womens-dropdown').style.visibility = "visible";
-            document.querySelector('.mens-dropdown').style.visibility = "hidden";
-            document.querySelector('.accessories-dropdown').style.visibility = "hidden";
-        }) 
-        
-        document.querySelector('.womens-dropdown').addEventListener('mouseover', () => {
-            document.querySelector('.womens-dropdown').style.visibility = "visible";
-            document.querySelector('.mens-dropdown').style.visibility = "hidden";
-            document.querySelector('.accessories-dropdown').style.visibility = "hidden";
-        })  
-        
-        document.querySelector('.womens-dropdown').addEventListener('mouseleave', () => {
-            document.querySelector('.womens-dropdown').style.visibility = "hidden";
-        }) 
-    }
-
-    if(i === 2){
-        nav_children[i].addEventListener('mouseover', () => {
-            document.querySelector('.accessories-dropdown').style.visibility = "visible";
-            document.querySelector('.mens-dropdown').style.visibility = "hidden";
-            document.querySelector('.womens-dropdown').style.visibility = "hidden";
-        })
-
-        document.querySelector('.womens-dropdown').addEventListener('mouseover', () => {
-            document.querySelector('.accessories-dropdown').style.visibility = "visible";
-            document.querySelector('.mens-dropdown').style.visibility = "hidden";
-            document.querySelector('.womens-dropdown').style.visibility = "hidden";
-        })  
-        
-        document.querySelector('.accessories-dropdown').addEventListener('mouseleave', () => {
-            document.querySelector('.accessories-dropdown').style.visibility = "hidden";
-        }) 
-    }
-
-}
-
-*/
