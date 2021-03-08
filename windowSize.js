783b52fa0ff1108c61fa022170239379ca766a91
br8S <56180503+br8S@users.noreply.github.com>
2021-03-08 14:07:05 -0800

    added changes to size of content for moboel view

--- windowSize.js
+++ windowSize.js
@@ -19,7 +19,7 @@ else {
         box.style.height = height + "vh";
         box.style.width = width + "vh";
 
-        document.querySelector('.content').style.height = height*6 + "vh";
+        document.querySelector('.content').style.height = height*6.3 + "vh";
     })
 }
 
@@ -44,7 +44,7 @@ window.onresize = function (event) {
             box.style.height = height + "vh";
             box.style.width = width + "vh";
 
-            document.querySelector('.content').style.height = height*6 + "vh";
+            document.querySelector('.content').style.height = height*6.3 + "vh";
         })
     }
 }
